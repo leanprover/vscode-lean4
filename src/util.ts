404fc68cdd56ca5fd5cd4f7edda59ba36ddaaaf4
Jared Roesch <roeschinc@gmail.com>
2016-12-19 16:46:58 -0800

    Fix exeception from identifierStart

--- src/util.ts
+++ src/util.ts
@@ -2,5 +2,12 @@ import {TextDocument, Position} from 'vscode';
 
 export function identifierStart(document : TextDocument, position : Position) : Position {
     let wordRange = document.getWordRangeAtPosition(position);
-    return wordRange.start;
+    // If we can't match the start of a word, we should just return the original point.
+    //
+    // The server will then return a null-record and the hover won't be displayed.
+    if (wordRange === undefined) {
+        return position;
+    } else {
+        return wordRange.start;
+    }
 }
