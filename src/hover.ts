0ca4afd88405f6bdf30cfcd08879751d6708b433
Jared Roesch <roeschinc@gmail.com>
2016-12-19 16:53:15 -0800

    Fix hover: `undefined : undefined`

--- src/hover.ts
+++ src/hover.ts
@@ -1,3 +1,5 @@
+'use strict';
+
 import * as vscode from 'vscode';
 import {Server} from './server';
 import {HoverProvider, Hover, TextDocument, Position, CancellationToken} from 'vscode';
@@ -13,9 +15,13 @@ export class LeanHoverProvider implements HoverProvider {
     public provideHover(document : TextDocument, position : Position, CancellationToken) : Thenable<Hover> {
         let startPos = util.identifierStart(document, position);
         return this.server.info(document.fileName, startPos.line + 1, startPos.character).then((response) => {
-            if (response.record) {
+            // Maybe use more sohpisticated typing here?
+            if (response.record && response.record['full-id']) {
                 let msg = response.record['full-id'] + ' : ' + response.record['type'];
                 let marked = { language: 'lean', value: msg };
+                return new Hover(marked, new vscode.Range(position.line, position.character, position.line, position.character));
+            } else if (response.record && response.record.state) {
+                let marked = { language: 'lean', value: response.record.state };
                 return new Hover(marked, new vscode.Range(position.line, position.character, position.line, position.character));
             } else {
                 return null;
