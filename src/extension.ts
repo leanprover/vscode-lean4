f0c9c4d74164760d146baa6b5362bd6e502bc64b
Gabriel Ebner <gebner@gebner.org>
2017-05-05 16:21:47 +0200

    Move task gutter and messages out of main extension file.

--- src/extension.ts
+++ src/extension.ts
@@ -14,13 +14,12 @@ import {ErrorViewProvider} from './errorview';
 import {LeanDiagnosticsProvider} from './diagnostics';
 import {LeanSyncService} from './sync';
 import {Message} from 'lean-client-js-node';
+import { LeanTaskGutter, LeanTaskMessages } from "./taskgutter";
 
 const LEAN_MODE : vscode.DocumentFilter = {
     language: "lean",
     scheme: 'file'
 }
-
-let taskMessages: vscode.DiagnosticCollection;
 
 // Seeing .olean files in the source tree is annoying, we should
 // just globally hide them.
@@ -66,25 +65,10 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(new LeanDiagnosticsProvider(server));
 
     // Task messages.
-    taskMessages = vscode.languages.createDiagnosticCollection('lean-tasks');
-    context.subscriptions.push(taskMessages);
-    context.subscriptions.push(server.statusChanged.on((status) => {
-        const diagsPerFile = new Map<string, vscode.Diagnostic[]>();
-
-        if (vscode.workspace.getConfiguration('lean').get('progressMessages')) {
-            for (const task of status.tasks) {
-                let diags = diagsPerFile.get(task.file_name);
-                if (!diags) diagsPerFile.set(task.file_name, diags = []);
-                diags.push(new vscode.Diagnostic(
-                    new vscode.Range(task.pos_line - 1, task.pos_col, task.end_pos_line - 1, task.end_pos_col),
-                    task.desc, vscode.DiagnosticSeverity.Information,
-                ));
-            }
-        }
-
-        taskMessages.clear();
-        diagsPerFile.forEach((diags, file) => taskMessages.set(vscode.Uri.file(file), diags));
-    }));
+    context.subscriptions.push(
+        new LeanTaskGutter(server, context),
+        new LeanTaskMessages(server),
+    );
 
     // Register the support for hovering.
     context.subscriptions.push(
@@ -144,37 +128,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Add item to the status bar.
     context.subscriptions.push(new LeanStatusBarItem(server, roiManager));
-
-    let taskDecoration = vscode.window.createTextEditorDecorationType({
-        overviewRulerLane: vscode.OverviewRulerLane.Left,
-        overviewRulerColor: 'rgba(255, 165, 0, 0.5)',
-        dark: {
-            gutterIconPath: context.asAbsolutePath('images/progress-dark.svg'),
-        },
-        light: {
-            gutterIconPath: context.asAbsolutePath('images/progress-light.svg'),
-        },
-        gutterIconSize: 'contain',
-    });
-
-    server.statusChanged.on((serverStatus) => {
-        for (let editor of vscode.window.visibleTextEditors) {
-            let ranges: vscode.DecorationOptions[] = [];
-            for (let task of serverStatus.tasks) {
-                if (task.file_name == editor.document.fileName) {
-                    const range = new vscode.Range(
-                        task.pos_line - 1, task.pos_col,
-                        task.end_pos_line - 1, task.end_pos_col,
-                    );
-                    ranges.push({
-                        range,
-                        hoverMessage: task.desc,
-                    });
-                }
-            }
-            editor.setDecorations(taskDecoration, ranges);
-        }
-    });
 
     const errorViewProvider = new ErrorViewProvider(server);
     context.subscriptions.push(
---
+++ src/taskgutter.ts
@@ -0,0 +1,80 @@
+import { Disposable, TextEditorDecorationType, window, OverviewRulerLane, ExtensionContext, Range, DecorationOptions, Diagnostic, DiagnosticCollection, languages, workspace, DiagnosticSeverity, Uri } from 'vscode';
+import { Server, ServerStatus } from './server';
+
+export class LeanTaskGutter implements Disposable {
+    private decoration: TextEditorDecorationType;
+    private subscriptions: Disposable[] = [];
+
+    constructor(server: Server, context: ExtensionContext) {
+        this.decoration = window.createTextEditorDecorationType({
+            overviewRulerLane: OverviewRulerLane.Left,
+            overviewRulerColor: 'rgba(255, 165, 0, 0.5)',
+            dark: {
+                gutterIconPath: context.asAbsolutePath('images/progress-dark.svg'),
+            },
+            light: {
+                gutterIconPath: context.asAbsolutePath('images/progress-light.svg'),
+            },
+            gutterIconSize: 'contain',
+        });
+    
+        this.subscriptions.push(server.statusChanged.on(
+            (status) => this.updateDecos(status)));
+    }
+
+    private updateDecos(status: ServerStatus) {
+        for (const editor of window.visibleTextEditors) {
+            editor.setDecorations(this.decoration,
+                status.tasks.filter((t) => t.file_name === editor.document.fileName)
+                    .map((task) => ({
+                        range: new Range(
+                            task.pos_line - 1, task.pos_col,
+                            task.end_pos_line - 1, task.end_pos_col,
+                        ),
+                        hoverMessage: task.desc,
+                    })));
+        }
+    }
+
+    dispose() {
+        this.decoration.dispose();
+        for (const s of this.subscriptions) s.dispose();
+    }
+}
+
+export class LeanTaskMessages implements Disposable {
+    private collection: DiagnosticCollection;
+    private subscriptions: Disposable[] = [];
+
+    constructor(server: Server) {
+        this.collection = languages.createDiagnosticCollection('lean-tasks');
+        this.subscriptions.push(server.statusChanged.on(
+            (status) => this.updateMsgs(status)));
+        this.subscriptions.push(workspace.onDidChangeConfiguration(() =>
+            this.updateMsgs(server.statusChanged.currentValue)));
+    }
+
+    private updateMsgs(status: ServerStatus) {
+        const diagsPerFile = new Map<string, Diagnostic[]>();
+
+        if (workspace.getConfiguration('lean').get('progressMessages')) {
+            for (const task of status.tasks) {
+                let diags = diagsPerFile.get(task.file_name);
+                if (!diags) diagsPerFile.set(task.file_name, diags = []);
+                diags.push(new Diagnostic(
+                    new Range(task.pos_line - 1, task.pos_col,
+                        task.end_pos_line - 1, task.end_pos_col),
+                    task.desc, DiagnosticSeverity.Information,
+                ));
+            }
+        }
+
+        this.collection.clear();
+        diagsPerFile.forEach((diags, file) => this.collection.set(Uri.file(file), diags));
+    }
+
+    dispose() {
+        this.collection.dispose();
+        for (const s of this.subscriptions) s.dispose();
+    }
+}
