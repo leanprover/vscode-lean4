29998ad17be4b26454a485f11702b861936a794d
Johannes Hölzl <johannes.hoelzl@gmx.de>
2017-06-14 18:11:13 -0400

    Simplify Info-View: only show the current state and errors

--- src/infoview.ts
+++ src/infoview.ts
@@ -5,183 +5,12 @@ import {
     workspace, window, commands, languages
 } from 'vscode';
 import { Server } from './server';
-import { InfoRecord } from "lean-client-js-node";
+import { InfoRecord, Message } from "lean-client-js-node";
 
-class InfoDocument implements Disposable {
-    private changedEmitter = new EventEmitter<any>();
-    readonly onChanged = this.changedEmitter.event;
-
-    private subscriptions: Disposable[] = [];
-
-    private paused: boolean = false;
-    private showMessages: boolean = true;
-
-    private fileName: string = null;
-    private goalPosition: Position = null;
-
-    private togglePauseCommand = 'lean.infoDocument.togglePause';
-    private toggleMessagesCommand = 'lean.infoDocument.toggleMessages';
-
-    private contents: string = '';
-
-    constructor(private server: Server, private leanDocs: DocumentSelector, private context : ExtensionContext) {
-        this.render();
-        this.subscriptions.push(
-            this.server.allMessages.on(() => this.rerender()),
-            this.server.statusChanged.on(() => this.updateGoal()),
-            window.onDidChangeTextEditorSelection(() => this.updatePosition()),
-            commands.registerCommand(this.togglePauseCommand, () => this.togglePause()),
-            commands.registerCommand(this.toggleMessagesCommand, () => {
-                this.showMessages = !this.showMessages;
-                this.rerender();
-            }),
-        );
-    }
-
-    get text() {
-        return this.contents;
-    }
-
-    dispose() {
-        for (const s of this.subscriptions) s.dispose();
-    }
-
-    private render() {
-        this.contents = `<!DOCTYPE html>
-            <html>
-            <head> 
-            <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-            <link rel="stylesheet" type="text/css" href="${this.getMediaPath("infoview.css")}">
-            <script src="${this.getMediaPath("infoview.js")}"></script>
-            </head>
-            <body class="scrollBeyondLastLine">` +
-                this.renderHeader() +
-                this.renderGoal() +
-                `<div id="messages">` + this.renderMessages() + `</div>`+
-            `</body></html>`;
-    }
-
-	private getMediaPath(mediaFile: string): string {
-		return Uri.file(this.context.asAbsolutePath(join('media', mediaFile))).toString();
-	}
-
-    private renderHeader() {
-        const cfg = workspace.getConfiguration("editor");
-        const points: string[] = [];
-        points.push((this.paused ? 'updates paused' : 'actively updating') + 
-            ` <a href="command:${this.togglePauseCommand}">[${this.paused ? 'continue' : 'pause'}]</a>`);
-        points.push((this.showMessages ? 'showing' : 'hiding') + ' error messages' +
-            ` <a href="command:${this.toggleMessagesCommand}">[${this.showMessages ? 'hide' : 'show'}]</a>`);
-        return `<div id="debug"> </div>
-            <div id="settings"> <span> ${points.map((p) => `${p}`).join('<br>')} </span> </div>`;
-    }
-
-    private updatePosition() {
-        if (!languages.match(this.leanDocs, window.activeTextEditor.document))
-            return;
-
-        const oldFile = this.fileName;
-        const oldPos = this.goalPosition;
-
-        this.fileName = window.activeTextEditor.document.fileName;
-        this.goalPosition = window.activeTextEditor.selection.active;
-
-        if (this.fileName !== oldFile) {
-            this.curGoal = null;
-            this.rerender();
-        }
-        else if (!this.goalPosition.isEqual(oldPos)) {
-            this.updateGoal();
-        }
-    }
-
-    private curGoal: {
-        file: string,
-        line: number,
-        column: number,
-        state: string,
-    };
-
-    private updateGoal() {
-        if (this.fileName && this.goalPosition) {
-            const file = this.fileName;
-            const line = this.goalPosition.line + 1;
-            const column = this.goalPosition.character;
-            this.server.info(file, line, column).then((info) => {
-                if (info.record && info.record.state) {
-                    this.curGoal = {
-                        file, line, column,
-                        state: info.record.state,
-                    };
-                    commands.executeCommand('_workbench.htmlPreview.postMessage',
-				        Uri.parse('lean-info:goals'),
-				        {
-                            command: 'goal',
-                            basename: basename(file),
-                            line: line,
-                            column: column,
-                            state: info.record.state
-				        });
-                } else {
-                    // there is no goal information maybe the infoview has some messages
-                    commands.executeCommand('_workbench.htmlPreview.postMessage',
-				        Uri.parse('lean-info:goals'),
-				        {
-                            command: 'reveal',
-                            line: this.goalPosition.line + 1,
-                            column: this.goalPosition.character
-				        });
-                }
-            });
-        } else {
-            this.curGoal = null;
-            commands.executeCommand('_workbench.htmlPreview.postMessage',
-                Uri.parse('lean-info:goals'),
-                {
-                    command: 'clear-goal'
-                });
-        }
-    }
-
-    private renderGoal() {
-        const curGoal = this.curGoal;
-        if (!curGoal) return ``;
-        const f = curGoal.file; const l = curGoal.line; const c = curGoal.column;
-        return `<div id="goal">
-                <h1>Current Goal at ${basename(f)}:${l}:${c}</a></h1>
-                <pre>${curGoal.state}</pre>
-            </div>`;
-    }
-
-    private renderMessages() {
-        if (!this.fileName || !this.showMessages) return ``;
-        const msgs = this.server.messages
-            .filter((m) => m.file_name === this.fileName)
-            .sort((a, b) => a.pos_line === b.pos_line
-                ? a.pos_col - b.pos_col
-                : a.pos_line - b.pos_line);
-        return msgs.map((m) =>
-            `<div class="message ${m.severity}">
-                <h1 title="${m.file_name}:${m.pos_line}:${m.pos_col}">
-                  <a href="${encodeURI('command:lean.revealPosition?' + JSON.stringify([Uri.file(m.file_name), m.pos_line - 1, m.pos_col]))}">
-                    ${basename(m.file_name)}:${m.pos_line}:${m.pos_col}: ${m.severity} ${m.caption}
-                  </a>
-                </h1>
-                <pre>${m.text}</pre>
-            </div>`).join("\n");
-    }
-
-    private rerender() {
-        if (this.paused) return;
-        this.render();
-        this.changedEmitter.fire();
-    }
-
-    private togglePause() {
-        this.paused = !this.paused;
-        this.render();
-        this.changedEmitter.fire();
-    }
+function compareMessages(m1: Message, m2: Message) : boolean {
+    return (m1.file_name == m2.file_name &&
+        m1.pos_line == m2.pos_line && m1.pos_col == m2.pos_col &&
+        m1.severity == m2.severity && m1.caption == m2.caption && m1.text == m2.text);
 }
 
 export class InfoProvider implements TextDocumentContentProvider, Disposable {
@@ -189,46 +18,199 @@ export class InfoProvider implements TextDocumentContentProvider, Disposable {
 
     private changedEmitter = new EventEmitter<Uri>();
     onDidChange = this.changedEmitter.event;
-    private documents = new Map<string, InfoDocument>();
+
     private subscriptions: Disposable[] = [];
 
+    private curFileName  : string = null;
+    private curPosition  : Position = null;
+    private curGoalState : string = null;
+    private curMessages  : Message[] = null;
+
     constructor(private server: Server, private leanDocs: DocumentSelector, private context : ExtensionContext) {
-        this.subscriptions.push(workspace.onDidCloseTextDocument((doc) => {
-            const uri = doc.uri.toString();
-            if (this.documents.has(uri)) {
-                this.documents.get(uri).dispose();
-                this.documents.delete(uri);
-            }
-        }));
         this.subscriptions.push(
-            commands.registerCommand('lean.revealPosition', (uri: Uri, line: number, column: number) => {
-        		for (let editor of window.visibleTextEditors) {
-		        	if (editor.document.uri.toString() === uri.toString()) {
-                        let pos = new Position(line, column);
-                        window.showTextDocument(editor.document);
-        				editor.revealRange(new Range(pos, pos), TextEditorRevealType.InCenterIfOutsideViewport);
-                        editor.selection = new Selection(pos, pos);
-                    }
-		        }
-            })
+            this.server.allMessages.on(() => this.updateMessages()),
+            this.server.statusChanged.on(() => this.updateGoal()),
+            window.onDidChangeTextEditorSelection(() => this.updatePosition())
         );
+        this.updatePosition();
     }
 
     dispose() {
-        this.documents.forEach((v) => v.dispose());
         for (const s of this.subscriptions) s.dispose();
     }
 
     provideTextDocumentContent(uri: Uri, token: CancellationToken): string {
-        const uriString = uri.toString();
-        if (!this.documents.has(uriString)) {
-            if (this.leanGoalsUri.toString() !== uriString) {
-                throw new Error(`unsupported uri: ${uri}`);
+        if (uri.toString() == this.leanGoalsUri.toString()) {
+            const content = this.render();
+            return content;
+        } else
+            throw new Error(`unsupported uri: ${uri}`);
+    }
+
+    private fire() {
+        this.changedEmitter.fire(this.leanGoalsUri);
+    }
+
+    private revealEditorPosition(uri: Uri, line: number, column: number) {
+        for (let editor of window.visibleTextEditors) {
+            if (editor.document.uri.toString() === uri.toString()) {
+                let pos = new Position(line, column);
+                window.showTextDocument(editor.document);
+                editor.revealRange(new Range(pos, pos), TextEditorRevealType.InCenterIfOutsideViewport);
+                editor.selection = new Selection(pos, pos);
             }
-            const doc = new InfoDocument(this.server, this.leanDocs, this.context);
-            doc.onChanged(() => this.changedEmitter.fire(uri));
-            this.documents.set(uriString, doc);
         }
-        return this.documents.get(uriString).text;
+    }
+
+    private updatePosition() {
+        if (!languages.match(this.leanDocs, window.activeTextEditor.document))
+            return;
+
+        const oldFileName = this.curFileName;
+        const oldPosition = this.curPosition;
+
+        this.curFileName = window.activeTextEditor.document.fileName;
+        this.curPosition = window.activeTextEditor.selection.active;
+
+        const f = this.curFileName;
+        const l = this.curPosition.line + 1;
+        const c = this.curPosition.character;
+        if (this.curFileName !== oldFileName || !this.curPosition.isEqual(oldPosition)) {
+            this.updateMessages();
+            this.updateGoal();
+            this.fire();
+        }
+    }
+
+    private updateMessages() {
+        if (!this.curFileName) return;
+        const msgs = this.server.messages
+            .filter((m) => m.file_name === this.curFileName &&
+                m.pos_line == this.curPosition.line + 1 &&
+                m.pos_col == this.curPosition.character);
+        if (!this.curMessages) {
+            this.curMessages = msgs;
+            this.fire();
+            return;
+        }
+        const old_msgs = this.curMessages;
+        if (msgs.length == old_msgs.length) {
+            let eq = true;
+            for (let i = 0; i < msgs.length; i++) {
+                if (!compareMessages(msgs[i], old_msgs[i])) {
+                    eq = false;
+                    break;
+                }
+            }
+            if (eq) return;
+        }
+        this.curMessages = msgs;
+    }
+
+    private updateGoal() {
+        const f = this.curFileName;
+        const l = this.curPosition.line + 1;
+        const c = this.curPosition.character;
+        this.server.info(f, l, c).then((info) => {
+            if (info.record && info.record.state) {
+                if (this.curGoalState !== info.record.state) {
+                    this.curGoalState = info.record.state;
+                    this.fire();
+                }
+            } else {
+                if (this.curGoalState) {
+                    this.curGoalState = null;
+                    this.fire();
+                }
+            }
+        })
+
+    }
+
+	private getMediaPath(mediaFile: string): string {
+		return Uri.file(this.context.asAbsolutePath(join('media', mediaFile))).toString();
+	}
+
+    private render() {
+        return `<!DOCTYPE html>
+            <html>
+            <head> 
+            <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+            <style>
+            div
+{
+  display: block;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+
+h1
+{
+  display: block;
+  width: 100%;
+
+  margin-bottom: 1ex;
+  margin-top: 1ex;
+  border-bottom: 1px solid black;
+  padding: 0px;
+
+  font-weight: normal;
+  font-size: 100%;
+}
+
+div.error h1 {
+  color: red;
+}
+
+div.warning h1 {
+  color: darkorange;
+}
+
+div.information h1 {
+  color: darkgreen;
+}
+
+#messages div {
+  border-top: 5px solid transparent;
+}
+
+.vscode-light h1 {
+  border-bottom: 1px solid black;
+}
+
+.vscode-dark h1 {
+  border-bottom: 1px solid white;
+}
+
+.vscode-dark div.highlight {
+  background: darkolivegreen;
+}
+
+            </style>
+            </head>
+            <body>` +
+                this.renderGoal() +
+                `<div id="messages">` + this.renderMessages() + `</div>`+
+            `</body></html>`;
+    }
+
+    private renderGoal() {
+        const goalState = this.curGoalState;
+        if (!goalState) return '';
+        return `<div id="goal"><h1>Tactic State</h1><pre>${goalState}</pre></div>`;
+    }
+
+    private renderMessages() {
+        if (!this.curFileName) return ``;
+        return this.curMessages.map((m) => {
+            const i = m.text.indexOf("\n");
+            if (i == -1) {
+                return `<div class="message ${m.severity}"><h1>${m.severity}: ${m.caption}</h1><pre>${m.text}</pre></div>`
+            }
+            return `<div class="message ${m.severity}">
+                <h1>${m.severity}: ${m.caption} ${m.text.substring(0, i)}</h1>
+                <pre>${m.text.substring(i)}</pre>
+            </div>`}).join("\n");
     }
 }
