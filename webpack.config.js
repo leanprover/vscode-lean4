7955b2d04d15842e2395fb71f9481c9572fa8b4b
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2020-05-10 19:35:42 +0100

    Move compile to use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -43,8 +43,45 @@ function getWebviewConfig(env) {
 	return webview;
 }
 
+function getExtensionConfig(env) {
+	let config = {
+		name: 'extension',
+		mode: env.production ? 'production' : 'development',
+		target: 'node',
+		entry: {
+			extension: './src/extension.ts'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+			alias: {
+				"node-fetch": path.resolve(__dirname, 'node_modules/node-fetch/lib/index.js'),
+			}
+		},
+		devtool: !env.production ? 'source-map' : undefined,
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'out'),
+			libraryTarget: "commonjs",
+			devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
+		},
+		externals: {
+			'vscode': 'commonjs vscode'
+		}
+	};
+	return config;
+}
+
 module.exports =  function(env) {
 	env = env || {};
 	env.production = !!env.production;
-	return [getWebviewConfig(env)];
+	return [getWebviewConfig(env), getExtensionConfig(env)];
 };
