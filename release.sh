f786ca3002993d22b022e5d828a10176eed96976
Gabriel Ebner <gebner@gebner.org>
2021-01-02 11:30:35 +0100

    Publish to Open VSX as well.

--- release.sh
+++ release.sh
@@ -4,13 +4,22 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
+if [ -z "$OVSX_PAT" ]; then
+  OVSX_PAT="$(pass pat/openvsx)" || exit 1
+  export OVSX_PAT
+fi
+
 set -ex
 new_version="$1"
 sed -i 's/"version": ".*"/"version": "'$new_version'"/' package.json
 npm i
 git commit -am "Release $new_version"
 git tag -a v$new_version -m "vscode-lean $new_version"
+
 ./node_modules/.bin/vsce publish
+
+./node_modules/.bin/ovsx publish
+
 git push
 git push --tags
 
