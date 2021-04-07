17c2d3768e091ae4ca8a42eeda22814aefb07a63
Gabriel Ebner <gebner@gebner.org>
2021-04-07 18:12:38 +0200

    Preserve focus when revealing output.

--- src/server.ts
+++ src/server.ts
@@ -96,7 +96,7 @@ export class Server extends leanclient.Server {
             switch (e.error) {
                 case 'stderr':
                     stderrOutput.append(e.chunk);
-                    stderrOutput.show();
+                    stderrOutput.show(true);
                     break;
                 case 'connect':
                     // json parsing errors
