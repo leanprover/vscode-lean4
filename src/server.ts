11f1baed9c4179e68d055c1b497effc7a6576dd7
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2021-04-17 11:21:08 -0400

    update elan URL

--- src/server.ts
+++ src/server.ts
@@ -180,7 +180,7 @@ export class Server extends leanclient.Server {
             // Now show the terminal and run elan.
             terminal.show();
             terminal.sendText(
-              'curl https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh -sSf | sh && ' +
+              'curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh && ' +
               'echo && read -n 1 -s -r -p "Press any key to start Lean" && exit\n');
         }
     }
