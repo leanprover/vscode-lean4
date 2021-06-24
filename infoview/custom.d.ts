f4c888929317b96b55122b341db996ae41604c25
Wojciech Nawrocki <t-wnawrocki@microsoft.com>
2021-06-24 15:04:19 -0700

    chore: remove infoview folder

--- infoview/custom.d.ts
+++
@@ -1,9 +0,0 @@
-// to get importing svg files to not error.
-declare module '*.png' {
-    const content: any;
-    export default content;
-}
-declare module '*.svg' {
-    const attributes: any;
-    const content: string;
-}
