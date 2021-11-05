e5a15f62896981fc8ef2a32a906313c82acd21cf
Gabriel Ebner <gebner@gebner.org>
2021-11-05 10:29:12 +0100

    Update lean4-infoview/src/infoview/contexts.ts
    Co-authored-by: Wojciech Nawrocki <wjnawrocki@protonmail.com>

--- lean4-infoview/src/infoview/contexts.ts
+++ lean4-infoview/src/infoview/contexts.ts
@@ -10,7 +10,7 @@ import { ServerVersion } from "./serverVersion";
 // Type-unsafe initializers for contexts which we immediately set up at the top-level.
 export const EditorContext = React.createContext<EditorConnection>(null as any);
 export const RpcContext = React.createContext<RpcSessions>(null as any);
-export const VersionContext = React.createContext<ServerVersion | undefined>(null as any);
+export const VersionContext = React.createContext<ServerVersion | undefined>(undefined);
 
 export const ConfigContext = React.createContext<InfoviewConfig>(defaultInfoviewConfig);
 export const LspDiagnosticsContext = React.createContext<Map<DocumentUri, Diagnostic[]>>(new Map());
