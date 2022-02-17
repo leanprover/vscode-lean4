bd90d146093a3793de5c5779db54cc9bf30e48ff
Chris Lovett <clovett@microsoft.com>
2022-02-17 02:02:08 -0800

    fix lint err

--- test/src/test/lean3/extension.test.ts
+++ test/src/test/lean3/extension.test.ts
@@ -23,7 +23,7 @@ suite('Extension Test Suite', () => {
 		assert(lean, 'Lean extension not loaded');
 		assert(!lean.exports.isLean4Project, 'Lean4 extension should not be running!');
 
-		console.log("Checking vscode commands...");
+		console.log('Checking vscode commands...');
 		const cmds = await vscode.commands.getCommands(true);
 		cmds.forEach(cmd => {
 			assert(cmd !== 'lean4.selectToolchain', 'Lean4 extension should not have any registered commands');
