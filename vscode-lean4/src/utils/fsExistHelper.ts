03c82e89e05c9d1acd1bd143ccacac1839e6cfaa
Mariana Alanis <malanistamez@microsoft.com>
2022-02-22 21:40:24 -0600

    Cleanup use of fs.existsSync()

--- vscode-lean4/src/utils/fsExistHelper.ts
+++ vscode-lean4/src/utils/fsExistHelper.ts
@@ -10,7 +10,7 @@ export async function fsExistHelper(pathFile: string): Promise<boolean> {
     Instead, user code should open/read/write the file directly and handle the error raised if the file does not exist.)
     */
 
-    const fileExists = await promises.access(this._temporaryStorage).then(() => true).catch(() => false);
+    const fileExists = await promises.access(pathFile).then(() => true).catch(() => false);
     if (!fileExists) {
           return false
     }
