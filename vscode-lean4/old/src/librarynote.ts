596e7c634e8ea3256c97be484f5ae105a7bb7dcb
Mariana Alanis <malanistamez@microsoft.com>
2022-02-23 12:54:23 -0600

    Add try-catch statement for read file possible exceptions

--- vscode-lean4/old/src/librarynote.ts
+++ vscode-lean4/old/src/librarynote.ts
@@ -17,17 +17,24 @@ export class LibraryNoteLinkProvider implements DocumentLinkProvider {
 
     async resolveDocumentLink(link: DocumentLink): Promise<DocumentLink> {
         const noteName = link.tooltip;
-        for (const leanFile of await workspace.findFiles('**/*.lean')) {
-            const content = (await workspace.fs.readFile(leanFile)).toString();
-            for (const m of content.matchAll(libraryNoteRegex)) {
-                console.log(m[1]);
-                if (m[1] === noteName) {
-                    const lineNo = content.substr(0, m.index).split(/\r\n|\r|\n/).length;
-                    link.target = leanFile.with({ fragment: `L${lineNo}` });
-                    return link;
+        // Adding try-catch statement for read file possible exceptions
+        try{
+            for (const leanFile of await workspace.findFiles('**/*.lean')) {
+                const content = (await workspace.fs.readFile(leanFile)).toString();
+                for (const m of content.matchAll(libraryNoteRegex)) {
+                    console.log(m[1]);
+                    if (m[1] === noteName) {
+                        const lineNo = content.substr(0, m.index).split(/\r\n|\r|\n/).length;
+                        link.target = leanFile.with({ fragment: `L${lineNo}` });
+                        return link;
+                    }
                 }
             }
+            await window.showErrorMessage(`Library note "${noteName}" not found.`);
         }
-        await window.showErrorMessage(`Library note "${noteName}" not found.`);
+        catch (ex){
+            // Throw an exception in case any other issue is encountered
+            window.showErrorMessage('Exception thrown: .' + ex);
+        }
     }
 }
