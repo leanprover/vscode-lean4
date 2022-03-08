4eccffb6a8993a5c4ff6ddda3853836c771c1d26
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-03-08 18:26:10 -0500

    refactor: move infoview from webpack to rollup

---
+++ lean4-infoview/rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import css from 'rollup-plugin-css-only';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/index.js',
+        sourcemap: true,
+        format: 'esm'
+    },
+    plugins: [
+        commonjs(),
+        typescript(),
+        nodeResolve({
+            browser: true
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            preventAssignment: true // TODO delete when `true` becomes the default
+        }),
+        css()
+    ]
+};
--- lean4-infoview/src/index.ts
+++ lean4-infoview/src/index.ts
@@ -5,3 +5,13 @@
  */
 export * from '@lean4/infoview-api';
 export { renderInfoview } from './infoview/main';
+
+/**
+ * Widget libraries which import @lean4/infoview components will also want to use React and related
+ * libraries. It turns out that re-exporting them here is the simplest way to provide a single,
+ * shared artifact to consumers.
+ */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as ReactPopper from 'react-popper';
+export { React, ReactDOM, ReactPopper }
