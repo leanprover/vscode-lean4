a68f0cfe9323f6dc45570dbdeadc58fbd331cdcd
Chris Lovett <clovett@microsoft.com>
2022-03-14 11:48:01 -0700

    make test not dependent on version numbers.

--- lean4-infoview/src/infoview/messages.tsx
+++ lean4-infoview/src/infoview/messages.tsx
@@ -35,7 +35,7 @@ const MessageView = React.memo(({uri, diag}: MessageViewProps) => {
                 <a className="link pointer mh2 dim codicon codicon-go-to-file"
                    onClick={e => { e.preventDefault(); void ec.revealLocation(loc); }}
                    title="reveal file location"></a>
-                <a className="link pointer mh2 dim codicon codicon-quote"
+                <a className="link pointer mh2 dim codicon codicon-quote copy-to-comment"
                    onClick={e => {e.preventDefault(); void ec.copyToComment(text)}}
                    title="copy message to comment"></a>
                 <a className="link pointer mh2 dim codicon codicon-clippy"
--- vscode-lean4/test/suite/info/info.test.ts
+++ vscode-lean4/test/suite/info/info.test.ts
@@ -10,22 +10,22 @@ suite('InfoView Test Suite', () => {
 
         console.log('=================== Copy to Comment ===================');
 
-        const lean = await initLean4Untitled('#eval Lean.versionString');
+        const lean = await initLean4Untitled('#eval 47*22');
         const info = lean.exports.infoProvider as InfoProvider;
 
-        await assertStringInInfoview(info, '4.0.0-nightly-');
+        await assertStringInInfoview(info, '1034');
 
         console.log('Clicking copyToComment button in InfoView');
-        await info.runTestScript('document.getElementsByClassName(\'codicon-quote\')[0].click()');
+        await info.runTestScript('document.getElementsByClassName(\'copy-to-comment\')[0].click()');
 
-        console.log("Checking editor contains '4.0.0-nightly'")
+        console.log("Checking editor contains '1034'")
         const editor = vscode.window.activeTextEditor;
         assert(editor !== undefined, 'no active editor');
-        await findWord(editor, '4.0.0-nightly');
+        await findWord(editor, '1034');
 
         console.log('make sure new text is selected');
         const text = editor.document.getText(editor.selection);
-        assert(text.indexOf('4.0.0-nightly') >= 0, 'copyToClipboard did not select the new text');
+        assert(text.indexOf('1034') >= 0, 'copy-to-comment did not select the new text');
 
     }).timeout(60000);
 
