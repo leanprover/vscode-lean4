9b69b2028e599b8dddc768085d7bc03123a810b7
Chris Lovett <clovett@microsoft.com>
2022-03-14 17:48:42 -0700

    revert unnecessary change

--- vscode-lean4/webview/index.ts
+++ vscode-lean4/webview/index.ts
@@ -5,7 +5,7 @@ import { Rpc } from '../src/rpc';
 
 const vscodeApi = acquireVsCodeApi();
 
-const rpc = new Rpc((m) => { vscodeApi.postMessage(m) });
+const rpc = new Rpc((m) => vscodeApi.postMessage(m));
 window.addEventListener('message', (e) => rpc.messageReceived(e.data))
 const editorApi: EditorApi = rpc.getApi();
 
