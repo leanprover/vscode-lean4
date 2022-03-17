3ecddd47d47b94058e51f42d2d1ac2fd527c40d4
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-03-17 16:22:17 -0400

    feat: update infoview README

---
+++ lean4-infoview/src/components.ts
@@ -0,0 +1,3 @@
+export { InteractiveCode } from './infoview/interactiveCode';
+export { InteractiveMessage } from './infoview/traceExplorer';
+export { EditorContext, RpcContext, VersionContext, ConfigContext, LspDiagnosticsContext, ProgressContext } from './infoview/contexts';
--- lean4-infoview/src/index.ts
+++ lean4-infoview/src/index.ts
@@ -1,7 +1,7 @@
 /**
- * This library is an editor-independent loader for the Lean 4 infoview. It expects to be hosted
- * within a webpage and connected to an editor through the main entrypoint, `renderInfoview`.
- * On being given this, the loader fetches its components and renders the infoview.
+ * See `README.md`.
+ * @module
  */
 export * from '@lean4/infoview-api';
+export * from './components';
 export { renderInfoview } from './infoview/main';
