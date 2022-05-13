c3c9ccf85e6f8ef1cddb9da6c445a21ffd7dad06
Mariana Alanis <malanistamez@microsoft.com>
2022-05-13 13:35:37 -0500

    Infoview doesn't show errors when lake fails

--- lean4-infoview/src/infoview/rpcInterface.ts
+++ lean4-infoview/src/infoview/rpcInterface.ts
@@ -134,13 +134,13 @@ export interface LineRange {
 
 export async function getInteractiveDiagnostics(rs: RpcSessions, pos: DocumentPosition, lineRange?: LineRange): Promise<InteractiveDiagnostic[] | undefined> {
     const ret = await rs.call<InteractiveDiagnostic[]>(pos, 'Lean.Widget.getInteractiveDiagnostics', {lineRange})
-    console.log('hola'+ret)
     if (ret) {
         for (const d of ret) {
             TaggedMsg_registerRefs(rs, pos, d.message)
         }
         return ret
     }
+    // in some cases 'ret' could be ''
     return undefined
 }
 
