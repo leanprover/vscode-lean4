b685c88ea58c0f25111750e719100505bba4287f
E.W.Ayers <edward.ayers@outlook.com>
2022-06-15 10:13:13 -0400

    feat: useAsyncUnthrottled

--- lean4-infoview/src/infoview/util.ts
+++ lean4-infoview/src/infoview/util.ts
@@ -281,22 +281,8 @@ export function mapRpcError(err : unknown) : Error {
 
 type Status = 'pending' | 'fulfilled' | 'rejected'
 
-/** This hook will run the given promise function whenever the deps change.
- * There will only ever be one in-flight promise at a time. If the deps
- * change while a promise is still in-flight, then the function will be run
- * again after the first promise has resolved.
- *
- * This function prevents race conditions if the requests resolve in a
- * different order to that which they were requested in:
- *
- * - Request 1 is sent with, say, line=42.
- * - Request 2 is sent with line=90.
- * - Request 2 returns with diags=[].
- * - Request 1 returns with diags=['error'].
- *
- * Without `useAsync` we would now return the diagnostics for line 42 even though we're at line 90.
- */
-export function useAsync<T>(fn : () => Promise<T>, deps : React.DependencyList = [])
+
+function useAsyncThrottled<T>(fn : () => Promise<T>, deps : React.DependencyList = [])
   : [Status, T | undefined, unknown | undefined] {
     const [result, setResult] = React.useState<T | undefined>(undefined)
     const [error, setError] = React.useState<unknown | undefined>(undefined)
@@ -335,7 +321,68 @@ export function useAsync<T>(fn : () => Promise<T>, deps : React.DependencyList =
       })
     }, [...deps, trig])
     return [status.current, result, error]
+}
+
+function useAsyncUnthrottled<T>(fn : () => Promise<T>, deps : React.DependencyList = []) : [Status, T | undefined, unknown | undefined] {
+  const idCount = React.useRef(0)
+  const latestResolved = React.useRef(0)
+  const [status, setStatus] = React.useState<Status>('pending')
+  const [error, setError] = React.useState<unknown>(undefined)
+  const [result, setResult] = React.useState<T | undefined>(undefined)
+  React.useEffect(() => {
+    idCount.current += 1
+    const taskId = idCount.current
+    setStatus('pending')
+    setError(undefined)
+    fn().then(result => {
+      if (latestResolved.current > taskId) {
+        return
+      }
+      setStatus('fulfilled')
+      setResult(result)
+      latestResolved.current = taskId
+    }, (err : any) => {
+      if (latestResolved.current > taskId) {
+        return
+      }
+      setStatus('rejected')
+      setError(err)
+      latestResolved.current = taskId
+    })
+  }, deps)
+  return [status, result, error]
+}
+
+/** This React hook will run the given promise function `fn` whenever the deps change
+ * and use it to update the status and result when the promise resolves.
+ *
+ * This function prevents race conditions if the requests resolve in a
+ * different order to that which they were requested in:
+ *
+ * - Request 1 is sent with, say, line=42.
+ * - Request 2 is sent with line=90.
+ * - Request 2 returns with diags=[].
+ * - Request 1 returns with diags=['error'].
+ *
+ * Without `useAsync` we would now return the diagnostics for line 42 even though we're at line 90.
+ *
+ * There is a 'throttled' and 'unthrottled' version of this function:
+ * - in _throttled_ `throttle = true`:
+ *   There will only ever be one in-flight promise at a time. If the deps
+ *   change while a promise is still in-flight, then `fn` will be run
+ *   again after the first promise has resolved.
+ * - in _unthrottled_ `throttle = false`:
+ *   `fn` will be fired as soon as the dependencies change. Each invocation of
+ *   `fn` is time ordered. If an earlier invocation resolves after a
+ *   later invocation (as happens in above example), then this result is discarded.
+ */
+export function useAsync<T>(fn : () => Promise<T>, deps : React.DependencyList = [], throttle = false): [Status, T | undefined, unknown | undefined] {
+  if (throttle) {
+    return useAsyncThrottled(fn, deps)
+  } else {
+    return useAsyncUnthrottled(fn, deps)
   }
+}
 
 /** `intersperse([x,y,z], a) ≡ [x,a,y,a,z]` */
 function intersperse<T>(items : T[], sep : T) : T[] {
