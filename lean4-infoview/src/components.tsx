8ff0ecc85757621226821eb2c08691e55c68af5f
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-06-22 21:00:16 -0400

    feat: export InteractiveCode

--- lean4-infoview/src/components.tsx
+++ lean4-infoview/src/components.tsx
@@ -10,6 +10,7 @@ export { EditorContext, RpcContext, VersionContext } from './infoview/contexts';
 export { EditorConnection } from './infoview/editorConnection';
 export { RpcSessions } from './infoview/rpcSessions';
 export { ServerVersion } from './infoview/serverVersion';
+export { InteractiveCode } from './infoview/interactiveCode';
 
 /** Display the given message data as interactive, pretty-printed text. */
 export function InteractiveMessageData({ pos, msg }: { pos: DocumentPosition, msg: MessageData }) {
