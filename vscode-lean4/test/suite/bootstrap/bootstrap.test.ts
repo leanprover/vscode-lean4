49f3d5ca35abf0a2f35094cafee929b22a5da176
Chris Lovett <clovett@microsoft.com>
2022-07-12 16:35:36 -0700

    fix: lint error.

--- vscode-lean4/test/suite/bootstrap/bootstrap.test.ts
+++ vscode-lean4/test/suite/bootstrap/bootstrap.test.ts
@@ -23,10 +23,10 @@ suite('Lean4 Bootstrap Test Suite', () => {
         assert(info, 'No InfoProvider export');
 
         // give it a extra long timeout in case test machine is really slow.
-        logger.log("Wait for elan install of Lean nightly build...")
+        logger.log('Wait for elan install of Lean nightly build...')
 		await waitForInfoviewHtml(info, '4.0.0-nightly-', 600);
 
-        logger.log("Lean installation is complete.")
+        logger.log('Lean installation is complete.')
 
         // make sure test is always run in predictable state, which is no file or folder open
         await closeAllEditors();
@@ -43,17 +43,17 @@ suite('Lean4 Bootstrap Test Suite', () => {
         const info = lean.exports.infoProvider;
         assert(info, 'No InfoProvider export');
 
-        logger.log("Wait for Lean nightly build server to start...")
+        logger.log('Wait for Lean nightly build server to start...')
 		await waitForInfoviewHtml(info, '4.0.0-nightly-', 60);
-        logger.log("Lean nightly build server is running.")
+        logger.log('Lean nightly build server is running.')
 
         // install table build which is also needed by subsequent tests.
-        logger.log("Wait for lean stable build to be installed...")
+        logger.log('Wait for lean stable build to be installed...')
 		await vscode.commands.executeCommand('lean4.selectToolchain', 'leanprover/lean4:stable');
 
         // give it a extra long timeout in case test machine is really slow.
 		await waitForInfoviewHtml(info, '4.0.0, commit', 600);
-        logger.log("Lean stable build is running.")
+        logger.log('Lean stable build is running.')
 		await vscode.commands.executeCommand('lean4.selectToolchain', 'reset');
 
         // make sure test is always run in predictable state, which is no file or folder open
@@ -66,13 +66,13 @@ suite('Lean4 Bootstrap Test Suite', () => {
         logger.log('=================== Create linked toolchain named master ===================');
         void vscode.window.showInformationMessage('Running tests: ' + __dirname);
 
-        logger.log("Create copy of nightly build in a temp master folder...")
+        logger.log('Create copy of nightly build in a temp master folder...')
         const elanRoot = getDefaultElanPath()
         const nightly = path.join(elanRoot, '..', 'toolchains', 'leanprover--lean4---nightly')
         const master = path.join(os.tmpdir(), 'lean4', 'toolchains', 'master')
         copyFolder(nightly, master);
 
-        logger.log("Use elan to link the master toolchain...")
+        logger.log('Use elan to link the master toolchain...')
         await batchExecute('elan', ['toolchain', 'link', 'master', master], null, undefined);
 
         // this will wait up to 60 seconds to do full elan lean install, so test machines better
@@ -81,14 +81,14 @@ suite('Lean4 Bootstrap Test Suite', () => {
         const info = lean.exports.infoProvider;
         assert(info, 'No InfoProvider export');
 
-        logger.log("Wait for stable lean server to start...")
+        logger.log('Wait for stable lean server to start...')
 		await vscode.commands.executeCommand('lean4.selectToolchain', 'leanprover/lean4:stable');
 		await assertStringInInfoview(info, '4.0.0, commit');
-        logger.log("Wait for master lean server to start...")
+        logger.log('Wait for master lean server to start...')
 		await vscode.commands.executeCommand('lean4.selectToolchain', 'master');
         // sometimes a copy of lean launches more slowly (especially on Windows).
         await waitForInfoviewHtml(info, '4.0.0-nightly-', 300);
-        logger.log("Linked master toolchain is running.")
+        logger.log('Linked master toolchain is running.')
 		await vscode.commands.executeCommand('lean4.selectToolchain', 'reset');
 
         // make sure test is always run in predictable state, which is no file or folder open
