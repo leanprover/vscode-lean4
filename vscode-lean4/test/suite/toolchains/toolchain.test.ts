01af739c62b8a6db4b8499791479756463fdf8d9
Chris Lovett <clovett@microsoft.com>
2022-07-14 00:20:12 -0700

    Test: more diagnostic output

--- vscode-lean4/test/suite/toolchains/toolchain.test.ts
+++ vscode-lean4/test/suite/toolchains/toolchain.test.ts
@@ -18,19 +18,23 @@ suite('Toolchain Test Suite', () => {
 		const lean = await initLean4Untitled('#eval Lean.versionString');
 		const info = lean.exports.infoProvider;
 		assert(info, 'No InfoProvider export');
-		// wait for infoView to show up.
-		await assertStringInInfoview(info, 'All Messages');
 
+		logger.log('wait for infoView to show up with 4.0.0-nightly-');
+		await assertStringInInfoview(info, 'All Messages');
 		await resetToolchain(lean.exports.clientProvider);
 
-		await assertStringInInfoview(info, '4.0.0-nightly-');
+		const expectedVersion = '4.0.0-nightly-'
+		const html = await waitForInfoviewHtml(info, expectedVersion);
+        const foundVersion = extractPhrase(html, expectedVersion, '\n')
 
-		// Now switch toolchains (simple suite uses leanprover/lean4:nightly by default)
 		const version = getAltBuildVersion()
+		logger.log(`Now switch toolchains to leanprover/lean4:${version}`);
 		await vscode.commands.executeCommand('lean4.selectToolchain', `leanprover/lean4:${version}`);
 		await assertStringInInfoview(info, version);
 
+		logger.log(`Resetting toolchain override and waiting for ${foundVersion}`);
 		await resetToolchain(lean.exports.clientProvider);
+		await assertStringInInfoview(info, foundVersion);
 
 		// make sure test is always run in predictable state, which is no file or folder open
 		await closeAllEditors();
@@ -43,12 +47,12 @@ suite('Toolchain Test Suite', () => {
 		void vscode.window.showInformationMessage('Running tests: ' + __dirname);
 
 		// Test we can restart the lean server
+		const simpleRoot = path.join(__dirname, '..', '..', '..', '..', 'test', 'test-fixtures', 'simple');
 
 		// run this code twice to ensure that it still works after a Restart Server
 		for (let i = 0; i < 2; i++) {
 
-			const testsRoot = path.join(__dirname, '..', '..', '..', '..', 'test', 'test-fixtures', 'simple');
-			const lean = await initLean4(path.join(testsRoot, 'Main.lean'));
+			const lean = await initLean4(path.join(simpleRoot, 'Main.lean'));
 
 			const info = lean.exports.infoProvider;
 			assert(info, 'No InfoProvider export');
@@ -57,10 +61,10 @@ suite('Toolchain Test Suite', () => {
 			const versionString = extractPhrase(html, 'Hello:', '<').trim();
 			logger.log(`>>> Found "${versionString}" in infoview`);
 
-			// Now invoke the restart server command
+			logger.log('Now invoke the restart server command');
 			const clients = lean.exports.clientProvider;
 			assert(clients, 'No LeanClientProvider export');
-			const client = clients.getClientForFolder(vscode.Uri.file(testsRoot));
+			const client = clients.getClientForFolder(vscode.Uri.file(simpleRoot));
 			if (client) {
 				await restartLeanServer(client);
 			} else {
@@ -95,7 +99,7 @@ suite('Toolchain Test Suite', () => {
 		// verify that we switched to different version
 		await waitForInfoviewHtml(info, version);
 
-		// Now reset the toolchain back to nightly build.
+		logger.log(`Resetting toolchain override to revert back to ${foundVersion}`);
 		await resetToolchain(lean.exports.clientProvider);
 
 		// Now make sure the reset works and we can go back to the previous nightly version.
@@ -136,12 +140,12 @@ suite('Toolchain Test Suite', () => {
 		const originalContents = fs.readFileSync(toolchainFile, 'utf8').toString();
 		assert(originalContents.trim() === 'leanprover/lean4:nightly');
 
-		// Switch to a alternate version by editing the toolchain file
 		const version = getAltBuildVersion()
+		logger.log(`Switch to a alternate version ${version} by editing the toolchain file`);
 		fs.writeFileSync(toolchainFile, `leanprover/lean4:${version}`);
 
 		try {
-			// verify that we switched to alt version
+			logger.log(`verify that we switched to alt version ${version}`);
 			const html = await assertStringInInfoview(info, version);
 
 			// check the path to lean.exe from the `eval IO.appPath`
@@ -154,9 +158,9 @@ suite('Toolchain Test Suite', () => {
 			const toolChains = await installer.elanListToolChains(null);
 			const masterToolChain = toolChains.find(tc => tc === 'master');
 			if (masterToolChain) {
-				// Switch to a linked toolchain version (setup in our workflow: on-push.yml)
+				logger.log('Switch to a linked toolchain master version');
 				fs.writeFileSync(toolchainFile, masterToolChain);
-				// verify that we switched to the master toolchain.
+				logger.log('verify that we switched to the master toolchain.');
 				await assertStringInInfoview(info, 'master');
 			}
 
@@ -165,7 +169,7 @@ suite('Toolchain Test Suite', () => {
 			fs.writeFileSync(toolchainFile, originalContents);
 		}
 
-		// Now make sure the reset works and we can go back to the previous nightly version.
+		logger.log(`Wait for 'master' version to appear, it should be ${foundVersion}`);
 		await assertStringInInfoview(info, foundVersion);
 
 		// make sure test is always run in predictable state, which is no file or folder open
