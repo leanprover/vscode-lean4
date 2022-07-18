0224d3f4576811ecf9e908b5651e9990ea0002a2
Mariana Alanis <malanistamez@microsoft.com>
2022-07-18 18:07:37 -0500

    fix: modify toolchain timeout

--- vscode-lean4/test/suite/utils/helpers.ts
+++ vscode-lean4/test/suite/utils/helpers.ts
@@ -345,7 +345,7 @@ export async function restartLeanServer(client: LeanClient, retries=60, delay=10
 
     while (count < retries){
         const index = stateChanges.indexOf('restarted');
-        if (index > 0) {
+        if (index >= 0) {
             break;
         }
         await sleep(delay);
@@ -353,10 +353,13 @@ export async function restartLeanServer(client: LeanClient, retries=60, delay=10
     }
 
     // check we have no errors.
-    const actual = stateChanges.toString();
-    const expected = 'stopped,restarted'
+    if (stateChanges.length === 0){
+        assert(false, 'restartServer did not fire any events')
+    }
+    const actual = stateChanges[stateChanges.length - 1];
+    const expected = 'restarted'
     if (actual !== expected) {
-        logger.log(`restartServer did not produce expected result: ${actual}`);
+        logger.log('restartServer did not generate restarted event');
     }
     assert(actual === expected);
     return false;
