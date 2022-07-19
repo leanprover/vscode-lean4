5c23ba0fd20e3c5246bd29703adb39c82cd96a1e
Mariana Alanis <malanistamez@microsoft.com>
2022-07-19 16:03:34 -0500

    fix: adjust timeout numbers

--- vscode-lean4/test/suite/toolchains/toolchain.test.ts
+++ vscode-lean4/test/suite/toolchains/toolchain.test.ts
@@ -196,7 +196,7 @@ suite('Toolchain Test Suite', () => {
 
 		// make sure test is always run in predictable state, which is no file or folder open
 		await closeAllEditors();
-	}).timeout(60000);
+	}).timeout(120000);
 
 	test('Edit lean-toolchain version', async () => {
 
@@ -264,6 +264,6 @@ suite('Toolchain Test Suite', () => {
 		// make sure test is always run in predictable state, which is no file or folder open
 		await closeAllEditors();
 
-	}).timeout(60000);
+	}).timeout(120000);
 
 }).timeout(60000);
