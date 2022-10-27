7138f581209ff2fabcd6220feee9c1de0160eb73
Chris Lovett <clovett@microsoft.com>
2022-10-27 13:15:56 -0700

    fix: try removing all hacks

--- vscode-lean4/test/suite/bootstrap/bootstrap.test.ts
+++ vscode-lean4/test/suite/bootstrap/bootstrap.test.ts
@@ -3,9 +3,8 @@ import * as os from 'os';
 import { suite } from 'mocha';
 import * as vscode from 'vscode';
 import { initLean4Untitled, waitForInfoviewHtml, closeAllEditors, waitForActiveClientRunning, waitForActiveClient,
-         getAltBuildVersion, assertStringInInfoview, copyFolder, extractPhrase, restartLeanServer } from '../utils/helpers';
+         getAltBuildVersion } from '../utils/helpers';
 import { logger } from '../../../src/utils/logger'
-import { LeanClient} from '../../../src/leanclient';
 
 suite('Lean4 Bootstrap Test Suite', () => {
 
@@ -22,14 +21,11 @@ suite('Lean4 Bootstrap Test Suite', () => {
 
         // give it a extra long timeout in case test machine is really slow.
         logger.log('Wait for elan install of Lean nightly build...')
-        const client = await waitForActiveClient(lean.exports.clientProvider, 120);
-        await waitForActiveClientRunning(lean.exports.clientProvider, 5, 60000, () => restartLeanServer(client));
+        await waitForActiveClient(lean.exports.clientProvider, 120);
+        await waitForActiveClientRunning(lean.exports.clientProvider, 120);
 
-        // This is a hack, we shouldn't need to do this, but there seems to be some sort of bootstrapping
-        // bug on a newly installed lean server, the first time info view appears it sometimes gets stuck.
-        // wait 60 seconds, if nothing then kick it with a restart server command, and try that up to 5 times
-        // and if it times out at 300 seconds then waitForInfoviewHtml prints the contents of the InfoView so we can see what happened.
-		await waitForInfoviewHtml(info, '4.0.0-nightly-', 5, 60000, false, () => restartLeanServer(client));
+        // if it times out at 300 seconds then waitForInfoviewHtml prints the contents of the InfoView so we can see what happened.
+		await waitForInfoviewHtml(info, '4.0.0-nightly-', 300);
 
         logger.log('Lean installation is complete.')
 
@@ -56,7 +52,7 @@ suite('Lean4 Bootstrap Test Suite', () => {
 		await waitForInfoviewHtml(info, '4.0.0-nightly-', 120);
         logger.log('Lean nightly build server is running.')
 
-        // install table build which is also needed by subsequent tests.
+        // install the alternate build
         logger.log(`Wait for lean4:${version} build to be installed...`)
 		await vscode.commands.executeCommand('lean4.selectToolchain', `leanprover/lean4:${version}`);
 
