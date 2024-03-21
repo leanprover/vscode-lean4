ffd4a610e5292c1b499ab8c8ae7a0c3a84236653
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2024-03-21 12:54:19 -0400

    chore: doc and rm ';'

--- lean4-infoview/src/infoview/event.ts
+++ lean4-infoview/src/infoview/event.ts
@@ -1,56 +1,67 @@
-import type { Disposable } from 'vscode-languageserver-protocol';
+import type { Disposable } from 'vscode-languageserver-protocol'
 
-/**
- * When `fire(...args)` is invoked on an `EventEmitter`, the provided `args` are propagated
- * to all registered handlers. Handlers can be registered using `on`. */
 export class EventEmitter<E> {
-    private handlers: ((_: E) => void)[] = [];
-    private handlersWithKey = new Map<string, ((_: E) => void)[]>();
-    current?: E;
+    private handlers: ((_: E) => void)[] = []
+    private handlersWithKey = new Map<string, ((_: E) => void)[]>()
+    current?: E
 
-    on(handler: (_: E) => void, key?: string | undefined): Disposable {
+    /** Register a handler for events emitted by this emitter
+     * and return a closure that removed the handler registration.
+     *
+     * If `key` is specified, only events fired with that key will propagate to this handler.
+     */
+    on(handler: (_: E) => void, key?: string): Disposable {
         if (key) {
-            const handlersForKey = this.handlersWithKey.get(key) ?? [];
-            handlersForKey.push(handler);
-            this.handlersWithKey.set(key, handlersForKey);
+            const handlersForKey = this.handlersWithKey.get(key) ?? []
+            handlersForKey.push(handler)
+            this.handlersWithKey.set(key, handlersForKey)
         } else {
-            this.handlers.push(handler);
+            this.handlers.push(handler)
         }
         return {
             dispose: () => {
                 if (key) {
-                    const handlersForKey = this.handlersWithKey.get(key) ?? [];
-                    this.handlersWithKey.set(key, handlersForKey.filter((h) => h !== handler));
+                    const handlersForKey = this.handlersWithKey.get(key) ?? []
+                    this.handlersWithKey.set(key, handlersForKey.filter(h => h !== handler))
                 } else {
-                    this.handlers = this.handlers.filter((h) => h !== handler);
+                    this.handlers = this.handlers.filter(h => h !== handler)
                 }
             }
-        };
+        }
     }
 
-    fire(event: E, key?: string | undefined): void {
-        this.current = event;
+    /**
+     * Propagate the event to registered handlers.
+     *
+     * The event is propagated to all keyless handlers.
+     * Furthermore if `key` is provided,
+     * the event is also propagated to handlers registered with that key.
+     */
+    fire(event: E, key?: string): void {
+        this.current = event
         for (const h of this.handlers) {
-            h(event);
+            h(event)
         }
         if (key) {
-            const handlersForKey = this.handlersWithKey.get(key) ?? [];
+            const handlersForKey = this.handlersWithKey.get(key) ?? []
             for (const h of handlersForKey) {
-                h(event);
+                h(event)
             }
         }
     }
 
     dispose(): void {
-        this.handlers = [];
+        this.handlers = []
     }
 }
 
 type ExcludeNonEvent<T, U> = T extends (...args: any) => Promise<void> ? U : never
 
 /**
- * Turn all fields in `T` which extend `(...args: As) => Promise<void>` into event emitter fields
- * `f: EventEmitter<As>`. Other fields are removed. */
+ * Turn all fields in `T` which extend `(...args: As) => Promise<void>`
+ * into event emitter fields `f: EventEmitter<As>`.
+ * Other fields are removed.
+ */
 export type Eventify<T> = {
     [P in keyof T as ExcludeNonEvent<T[P], P>]:
         T[P] extends (arg: infer A) => Promise<void> ? EventEmitter<A> :
