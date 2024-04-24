5bf78163ca38a716c68b9bb812d810c10a799f95
mhuisi <mhuisi@protonmail.com>
2024-04-24 18:18:05 +0200

    feat: add version queries to setup diagnostics

--- vscode-lean4/src/utils/setupDiagnostics.ts
+++ vscode-lean4/src/utils/setupDiagnostics.ts
@@ -1,6 +1,30 @@
+import { SemVer } from 'semver'
 import { OutputChannel } from 'vscode'
 import { batchExecute, ExecutionExitCode, ExecutionResult } from './batch'
 import { FileUri } from './exturi'
+
+export type VersionQueryResult =
+    | { kind: 'Success'; version: SemVer }
+    | { kind: 'CommandNotFound' }
+    | { kind: 'CommandError'; message: string }
+    | { kind: 'InvalidVersion' }
+
+export function versionQueryResult(executionResult: ExecutionResult, versionRegex: RegExp): VersionQueryResult {
+    if (executionResult.exitCode === ExecutionExitCode.CannotLaunch) {
+        return { kind: 'CommandNotFound' }
+    }
+
+    if (executionResult.exitCode === ExecutionExitCode.ExecutionError) {
+        return { kind: 'CommandError', message: executionResult.stderr }
+    }
+
+    const match = versionRegex.exec(executionResult.stdout)
+    if (!match) {
+        return { kind: 'InvalidVersion' }
+    }
+
+    return { kind: 'Success', version: new SemVer(match[1]) }
+}
 
 export class SetupDiagnoser {
     channel: OutputChannel
@@ -11,24 +35,25 @@ export class SetupDiagnoser {
         this.cwdUri = cwdUri
     }
 
-    async checkLakeAndDepsAvailable(): Promise<'Success' | 'LakeUnavailable' | 'GitUnavailable'> {
-        if (!(await this.checkLakeAvailable())) {
-            return 'LakeUnavailable'
-        }
-        if (!(await this.checkGitAvailable())) {
-            return 'GitUnavailable'
-        }
-        return 'Success'
-    }
-
     async checkLakeAvailable(): Promise<boolean> {
         const lakeVersionResult = await this.runSilently('lake', ['--version'])
         return lakeVersionResult.exitCode === ExecutionExitCode.Success
     }
 
     async checkGitAvailable(): Promise<boolean> {
-        const gitVersionResult = await batchExecute('git', ['--version'])
+        const gitVersionResult = await this.runSilently('git', ['--version'])
         return gitVersionResult.exitCode === ExecutionExitCode.Success
+    }
+
+    async queryLeanVersion(toolchain?: string | undefined): Promise<VersionQueryResult> {
+        const options = toolchain ? ['--version', '+' + toolchain] : ['--version']
+        const leanVersionResult = await this.runSilently('lean', options)
+        return versionQueryResult(leanVersionResult, /version (\d+\.\d+\.\d+)/)
+    }
+
+    async queryElanVersion(): Promise<VersionQueryResult> {
+        const elanVersionResult = await this.runSilently('elan', ['--version'])
+        return versionQueryResult(elanVersionResult, /elan (\d+\.\d+\.\d+)/)
     }
 
     private async runSilently(executablePath: string, args: string[]): Promise<ExecutionResult> {
