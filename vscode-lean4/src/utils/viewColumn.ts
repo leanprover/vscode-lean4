d4410a3259fefbf84b8a28f424a02db386a460a4
Marc Huisinga <mhuisi@protonmail.com>
2024-12-05 16:32:40 +0100

    fix: allow infoview to appear in locked tab groups

--- vscode-lean4/src/utils/viewColumn.ts
+++ vscode-lean4/src/utils/viewColumn.ts
@@ -9,7 +9,17 @@ export function viewColumnOfInfoView(): ViewColumn {
             return tabGroup.viewColumn
         }
     }
-    return ViewColumn.Beside
+
+    // We do not use `ViewColumn.Beside` here because `ViewColumn.Beside` will never
+    // add a tab to a locked tab group.
+    // This is especially problematic because locking the tab group of the InfoView
+    // is a workaround for https://github.com/microsoft/vscode/issues/212679
+    // and using `ViewColumn.Beside` will retain an empty locked tab group when restarting VS Code.
+    const activeColumn = window.activeTextEditor?.viewColumn
+    if (activeColumn === undefined) {
+        return ViewColumn.Two
+    }
+    return activeColumn + 1
 }
 
 export function viewColumnOfActiveTextEditor(): ViewColumn {
