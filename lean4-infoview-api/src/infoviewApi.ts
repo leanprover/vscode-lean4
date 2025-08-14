1fc20b7d9c22b4d11e1ae128e409efc5c819af44
Marc Huisinga <mhuisi@protonmail.com>
2025-08-14 11:40:06 +0200

    feat: re-add copy state/message functionality (#644)
    This PR re-adds the copy state/message to clipboard functionality after
    it was removed in #606. The functionality is available from the context
    menu (as the other "edit" webview actions are), not from an icon button.

--- lean4-infoview-api/src/infoviewApi.ts
+++ lean4-infoview-api/src/infoviewApi.ts
@@ -132,6 +132,8 @@ export type ContextMenuEntry =
     | 'refresh'
     | 'pauseAllMessages'
     | 'unpauseAllMessages'
+    | 'copyState'
+    | 'copyMessage'
     | 'goToPinnedLocation'
     | 'goToMessageLocation'
     | 'displayTargetBeforeAssumptions'
