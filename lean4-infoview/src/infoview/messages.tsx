leanprover/vscode-lean4

commit 64580eb4e837862c3db8451df2f056cd4c37dd51

feat: distinguish message tally by severity (#672)
This PR distinguishes the message counters of "Messages" and "All
Messages" in the InfoView by severity.

<img width="198" height="82" alt="image"
src="https://github.com/user-attachments/assets/4583c822-bdb5-42b9-a069-65b1df9fd658"
/>

Closes #574.

lean4-infoview/src/infoview/messages.tsx
@@ -21,6 +21,7 @@ import { InteractiveMessage } from './traceExplorer'
 import {
     addUniqueKeys,
     basename,
+    count,
     DocumentPosition,
     escapeHtml,
     Keyed,
@@ -383,7 +384,7 @@ export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
 
     // The number of actually displayed messages, or `undefined` if the panel is collapsed.
     // When `undefined`, we can approximate it by `diags.length`.
-    const [numDiags, setNumDiags] = React.useState<number | undefined>(undefined)
+    const [tally, setTally] = React.useState<DiagnosticTally | undefined>(undefined)
 
     const id = React.useId()
     useEvent(ec.events.clickedContextMenu, _ => setPaused(true), [], `pauseAllMessages:${id}`)
@@ -399,7 +400,7 @@ export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
                 data-vscode-context={JSON.stringify(context)}
             >
                 <>
-                    All Messages ({numDiags ?? diags.length})
+                    All Messages <TallyDisplay t={tally ?? tallyOfDiags(diags)}></TallyDisplay>
                     <span
                         className="fr"
                         onClick={e => {
@@ -433,38 +434,79 @@ export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
                         ></a>
                     </span>
                 </>
-                <AllMessagesBody
-                    uri={uri}
-                    messages={iDiags}
-                    setNumDiags={setNumDiags}
-                    sortOrder={sortOrder}
-                    pos={curPos}
-                />
+                <AllMessagesBody uri={uri} messages={iDiags} setTally={setTally} sortOrder={sortOrder} pos={curPos} />
             </Details>
         </RpcContext.Provider>
+    )
+}
+
+export interface DiagnosticTally {
+    errors: number
+    warnings: number
+    infos: number
+    total: number
+}
+
+export function tallyOfDiags(msgs: { severity?: DiagnosticSeverity }[]): DiagnosticTally {
+    return {
+        errors: count(msgs, m => m.severity === DiagnosticSeverity.Error),
+        warnings: count(msgs, m => m.severity === DiagnosticSeverity.Warning),
+        infos: count(msgs, m => m.severity === DiagnosticSeverity.Information),
+        total: msgs.length,
+    }
+}
+
+export function TallyDisplay({ t }: { t: DiagnosticTally }) {
+    if (t.errors === 0 && t.warnings === 0 && t.infos === 0) {
+        return <></>
+    }
+    const warningSpace = t.errors > 0 ? <> </> : <></>
+    const infoSpace = t.warnings > 0 || t.errors > 0 ? <> </> : <></>
+    return (
+        <>
+            (
+            {t.errors > 0 && (
+                <>
+                    <span className={'font-codicon codicon codicon-error'}></span> {t.errors}
+                </>
+            )}
+            {t.warnings > 0 && (
+                <>
+                    {warningSpace}
+                    <span className={'font-codicon codicon codicon-warning'}></span> {t.warnings}
+                </>
+            )}
+            {t.infos > 0 && (
+                <>
+                    {infoSpace}
+                    <span className={'font-codicon codicon codicon-info'}></span> {t.infos}
+                </>
+            )}
+            )
+        </>
     )
 }
 
 interface AllMessagesBodyProps {
     uri: DocumentUri
     messages: () => Promise<InteractiveDiagnostic[]>
-    setNumDiags: React.Dispatch<React.SetStateAction<number | undefined>>
+    setTally: React.Dispatch<React.SetStateAction<DiagnosticTally | undefined>>
     sortOrder: MessageOrder
     pos: DocumentPosition | undefined
 }
 
 /** We factor out the body of {@link AllMessages} which lazily fetches its contents only when expanded. */
-function AllMessagesBody({ uri, messages, setNumDiags, sortOrder, pos }: AllMessagesBodyProps) {
+function AllMessagesBody({ uri, messages, setTally, sortOrder, pos }: AllMessagesBodyProps) {
     const [msgs, setMsgs] = React.useState<InteractiveDiagnostic[] | undefined>(undefined)
     React.useEffect(() => {
         const fn = async () => {
             const msgs = await messages()
             setMsgs(msgs)
-            setNumDiags(msgs.length)
+            setTally(tallyOfDiags(msgs))
         }
         void fn()
-    }, [messages, setNumDiags])
-    React.useEffect(() => () => /* Called on unmount. */ setNumDiags(undefined), [setNumDiags])
+    }, [messages, setTally])
+    React.useEffect(() => () => /* Called on unmount. */ setTally(undefined), [setTally])
     if (msgs === undefined) return <>Loading messages...</>
     else return <MessagesList uri={uri} messages={msgs} sortOrder={sortOrder} pos={pos} />
 }

lean4-infoview/src/infoview/index.css
@@ -30,6 +30,13 @@ body {
     height: 16px;
 }
 
+.font-codicon {
+    font-weight: var(--vscode-editor-font-weight) !important;
+    font-size: var(--vscode-editor-font-size) !important;
+    line-height: var(--vscode-editor-line-height) !important;
+    vertical-align: middle;
+}
+
 /* These are syntax highlights for the string goal view. */
 .goal-goals {
     color: var(--vscode-lean4-infoView\.goalCount);

lean4-infoview/src/infoview/info.tsx
@@ -19,7 +19,7 @@ import { Details } from './collapsing'
 import { ConfigContext, EditorContext, EnvPosContext, LspDiagnosticsContext, ProgressContext } from './contexts'
 import { GoalsLocation, Locations, LocationsContext } from './goalLocation'
 import { FilteredGoals, goalsToString } from './goals'
-import { lspDiagToInteractive, MessagesList } from './messages'
+import { lspDiagToInteractive, MessagesList, TallyDisplay, tallyOfDiags } from './messages'
 import { RpcContext, useRpcSessionAtPos } from './rpcSessions'
 import { PanelWidgetDisplay } from './userWidget'
 import {
@@ -263,7 +263,9 @@ const InfoDisplayContent = React.memo((props: InfoDisplayContentProps) => {
             <GoalInfoDisplay pos={pos} goals={goals} termGoal={termGoal} userWidgets={userWidgets} />
             <div style={hasMessages ? {} : { display: 'none' }} key="messages">
                 <Details initiallyOpen key="messages">
-                    <>Messages ({messages.length})</>
+                    <>
+                        Messages <TallyDisplay t={tallyOfDiags(messages)}></TallyDisplay>
+                    </>
                     <div className="ml1">
                         <MessagesList
                             uri={pos.uri}

lean4-infoview/src/infoview/util.ts
@@ -8,6 +8,16 @@ import { isRpcError, RpcErrorCode } from '@leanprover/infoview-api'
 import { EditorContext } from './contexts'
 import { EventEmitter } from './event'
 
+export function count<T>(xs: T[], p: (v: T) => boolean): number {
+    let n = 0
+    for (const x of xs) {
+        if (p(x)) {
+            n++
+        }
+    }
+    return n
+}
+
 /** A document URI and a position in that document. */
 export interface DocumentPosition extends Position {
     uri: DocumentUri

commit ac86e113f30a7000713f4dcc3e1c17107c445aa5

fix: 'all messages' render loop (#686)
This PR fixes a significant regression introduced by #672 where opening
the 'All messages' section would cause the client to start spamming
requests for interactive diagnostics to the language server.

lean4-infoview/src/infoview/messages.tsx
@@ -334,6 +334,11 @@ function lazy<T>(f: () => T): () => T {
     }
 }
 
+interface InteractiveDiagBundle {
+    diags: InteractiveDiagnostic[]
+    tally: DiagnosticTally
+}
+
 /** Displays all messages for the specified file. Can be paused. */
 export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
     const ec = React.useContext(EditorContext)
@@ -361,7 +366,7 @@ export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
                     let diags = await getInteractiveDiagnostics(rs0, { start: 0, end: maxLine })
                     diags = diags.filter(d => d.isSilent === undefined || !d.isSilent)
                     if (diags.length > 0) {
-                        return diags
+                        return { diags, tally: tallyOfDiags(diags) }
                     }
                 } catch (err: any) {
                     if (err?.code === RpcErrorCode.ContentModified) {
@@ -373,7 +378,8 @@ export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
                         console.log('getInteractiveDiagnostics error ', err)
                     }
                 }
-                return diags0.map(d => ({ ...d, message: { text: d.message } }))
+                const diags = diags0.map(d => ({ ...d, message: { text: d.message } }))
+                return { diags, tally: tallyOfDiags(diags) }
             }),
         [rs0, diags0],
     )
@@ -451,7 +457,7 @@ export function AllMessages({ uri: uri0 }: { uri: DocumentUri }) {
                         ></a>
                     </span>
                 </>
-                <AllMessagesBody uri={uri} messages={iDiags} setTally={setTally} sortOrder={sortOrder} pos={curPos} />
+                <AllMessagesBody uri={uri} messages={iDiags} tally={tally} setTally={setTally} sortOrder={sortOrder} pos={curPos} />
             </Details>
         </RpcContext.Provider>
     )
@@ -506,23 +512,27 @@ export function TallyDisplay({ t }: { t: DiagnosticTally }) {
 
 interface AllMessagesBodyProps {
     uri: DocumentUri
-    messages: () => Promise<InteractiveDiagnostic[]>
+    messages: () => Promise<InteractiveDiagBundle>
+    tally: DiagnosticTally | undefined
     setTally: React.Dispatch<React.SetStateAction<DiagnosticTally | undefined>>
     sortOrder: MessageOrder
     pos: DocumentPosition | undefined
 }
 
 /** We factor out the body of {@link AllMessages} which lazily fetches its contents only when expanded. */
-function AllMessagesBody({ uri, messages, setTally, sortOrder, pos }: AllMessagesBodyProps) {
+function AllMessagesBody({ uri, messages, tally, setTally, sortOrder, pos }: AllMessagesBodyProps) {
     const [msgs, setMsgs] = React.useState<InteractiveDiagnostic[] | undefined>(undefined)
     React.useEffect(() => {
         const fn = async () => {
-            const msgs = await messages()
-            setMsgs(msgs)
-            setTally(tallyOfDiags(msgs))
+            const { diags, tally: newTally } = await messages()
+            setMsgs(diags)
+            // Avoid re-render loop with `AllMessages`
+            if (JSON.stringify(tally) !== JSON.stringify(newTally)) {
+                setTally(newTally)
+            }
         }
         void fn()
-    }, [messages, setTally])
+    }, [messages, tally, setTally])
     React.useEffect(() => () => /* Called on unmount. */ setTally(undefined), [setTally])
     if (msgs === undefined) return <>Loading messages...</>
     else return <MessagesList uri={uri} messages={msgs} sortOrder={sortOrder} pos={pos} />

vscode-lean4/src/infoview.ts
@@ -390,9 +390,7 @@ export class InfoProvider implements Disposable {
                 this.updateStylesheet()
                 await this.sendConfig()
             }),
-            workspace.onDidChangeTextDocument(async () => {
-                await this.sendPosition()
-            }),
+            lean.onDidChangeLeanDocument(() => this.sendPosition()),
             lean.registerLeanEditorCommand('lean4.displayGoal', leanEditor => this.openPreview(leanEditor)),
             commands.registerCommand('lean4.toggleInfoview', () => this.toggleInfoview()),
             lean.registerLeanEditorCommand('lean4.displayList', async leanEditor => {

vscode-lean4/src/utils/leanEditorProvider.ts
@@ -4,6 +4,7 @@ import {
     EventEmitter,
     ExtensionContext,
     TextDocument,
+    TextDocumentChangeEvent,
     TextEditor,
     TextEditorEdit,
     TextEditorSelectionChangeEvent,
@@ -130,6 +131,9 @@ export class LeanEditorProvider implements Disposable {
     private readonly onDidConcealLeanEditorEmitter = new EventEmitter<LeanEditor>()
     readonly onDidConcealLeanEditor = this.onDidConcealLeanEditorEmitter.event
 
+    private readonly onDidChangeLeanDocumentEmitter = new EventEmitter<TextDocumentChangeEvent>()
+    readonly onDidChangeLeanDocument = this.onDidChangeLeanDocumentsEmitter.event
+
     private readonly onDidChangeLeanEditorSelectionEmitter = new EventEmitter<TextEditorSelectionChangeEvent>()
     readonly onDidChangeLeanEditorSelection = this.onDidChangeLeanEditorSelectionEmitter.event
 
@@ -168,6 +172,7 @@ export class LeanEditorProvider implements Disposable {
                 this.invalidateClosedLastActiveLeanDocument(doc)
             }),
         )
+        this.subscriptions.push(workspace.onDidChangeTextDocument(event => this.updateDocument(event)))
         this.subscriptions.push(window.onDidChangeTextEditorSelection(event => this.updateTextEditorSelection(event)))
     }
 
@@ -305,6 +310,13 @@ export class LeanEditorProvider implements Disposable {
         }
         this._lastActiveLeanDocument = newLastActiveLeanDocument
         this.onDidChangeLastActiveLeanDocumentEmitter.fire(newLastActiveLeanDocument)
+    }
+
+    private updateDocument(event: TextDocumentChangeEvent) {
+        if (!this.isLeanDocument(event.document)) {
+            return
+        }
+        this.onDidChangeLeanDocumentEmitter.fire(event)
     }
 
     private updateTextEditorSelection(event: TextEditorSelectionChangeEvent) {