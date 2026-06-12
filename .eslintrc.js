a4968cd859254c991687f0a917a5e0c9dec0c5dc
Wojciech Nawrocki <wjn@lean-fro.org>
2026-06-12 15:43:57 -0400

    fix: eslint with worktrees

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ https://github.com/typescript-eslint/tslint-to-eslint-config/blob/master/docs/FA
 Happy linting! 💖
 */
 module.exports = {
+    root: true,
     env: {
         es6: true,
         node: true,
